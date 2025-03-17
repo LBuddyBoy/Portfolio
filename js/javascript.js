e105e6b2b0001a59a56ae804a409c91e16d20c13
Ethan Toups <ethantoups05@gmail.com>
2025-03-17 14:08:01 -0500

    Added compatibility for mobile

---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+const form = document.querySelector("#form");
+const submitButton = document.querySelector("#submit");
+
+form.addEventListener("submit", (e) => {
+  console.log(window.location.origin + "/success.html");
+  submitButton.disabled = true;
+  e.preventDefault();
+  window.location.href = window.location.origin + "/success.html";
+});
